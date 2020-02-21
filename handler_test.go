272d893c7563025dab3935c1a12f36e5990a2ac8
Piyush Verma <github@piyushverma.net>
2020-02-21 07:56:01 +0530

    Don't talk to kubernetes client

--- handler_test.go
+++ handler_test.go
@@ -5,9 +5,11 @@ import (
 	"io/ioutil"
 	"sync"
 	"testing"
+	"time"
 
 	"gopkg.in/go-playground/assert.v1"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 type events struct {
@@ -32,16 +34,17 @@ func TestMakeL9Event(t *testing.T) {
 	assert.Equal(t, ev.ID, "19b4506f-95f4-4dd0-8d2d-bf7647997877")
 	assert.Equal(t, ev.Address[0], "127.0.0.1")
 
-	kc, err := newK8sClient("testdata/kubeconfig")
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	mcache, err := cacheClient()
 	if err != nil {
 		t.Fatal(err)
 	}
-	mcache.Set("814a4994-e977-4e07-be69-6a464b2169c9", nil, 0)
+
+	mcache.Set(
+		"814a4994-e977-4e07-be69-6a464b2169c9",
+		&unstructured.Unstructured{}, 0,
+	)
+
+	time.Sleep(1) // Wait for cache to fill in
 
 	var wg sync.WaitGroup
 	ch := make(chan *L9Event)
@@ -52,7 +55,7 @@ func TestMakeL9Event(t *testing.T) {
 	}()
 
 	wg.Add(1)
-	h := &Handler{kc, ch, mcache}
+	h := &Handler{&kubernetesClient{}, ch, mcache}
 	h.OnAdd(e.Items[0])
 	wg.Wait()
 }
