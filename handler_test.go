9c41570d04444ee18cd8156c3ab5edd9d3490859
Piyush Verma <github@piyushverma.net>
2020-02-21 09:21:04 +0530

    Add a small delay in Cache

--- handler_test.go
+++ handler_test.go
@@ -17,45 +17,56 @@ type events struct {
 }
 
 func TestMakeL9Event(t *testing.T) {
-	e := &events{}
-	b, err := ioutil.ReadFile("testdata/events.log")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if err := json.Unmarshal(b, e); err != nil {
-		t.Fatal(err)
-	}
-
-	ev := makeL9Event(
-		e.Items[0], nil, []string{"127.0.0.1"},
-	)
-
-	assert.Equal(t, ev.ID, "19b4506f-95f4-4dd0-8d2d-bf7647997877")
-	assert.Equal(t, ev.Address[0], "127.0.0.1")
-
-	mcache, err := cacheClient()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	mcache.Set(
-		"814a4994-e977-4e07-be69-6a464b2169c9",
-		&unstructured.Unstructured{}, 0,
-	)
-
-	time.Sleep(1) // Wait for cache to fill in
-
 	var wg sync.WaitGroup
+	e := &events{}
 	ch := make(chan *L9Event)
-	go func() {
-		defer wg.Done()
-		x := <-ch
-		assert.Equal(t, "Scheduled", x.Reason)
-	}()
+	mCache, _ := cacheClient()
+	key := "814a4994-e977-4e07-be69-6a464b2169c9"
 
-	wg.Add(1)
-	h := &Handler{&kubernetesClient{}, ch, mcache}
-	h.OnAdd(e.Items[0])
-	wg.Wait()
+	t.Run("Set and Cache", func(t *testing.T) {
+		mCache.Set(key, &unstructured.Unstructured{}, 1)
+	})
+
+	t.Run("Wait till you get the key", func(t *testing.T) {
+		var ok bool
+		for i := 0; i < 5; i++ {
+			if _, ok = mCache.Get(key); ok {
+				break
+			}
+			time.Sleep(1)
+		}
+
+		assert.Equal(t, true, ok)
+	})
+
+	t.Run("Make Last9 Event", func(t *testing.T) {
+		b, err := ioutil.ReadFile("testdata/events.log")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if err := json.Unmarshal(b, e); err != nil {
+			t.Fatal(err)
+		}
+
+		ev := makeL9Event(
+			e.Items[0], nil, []string{"127.0.0.1"},
+		)
+
+		assert.Equal(t, ev.ID, "19b4506f-95f4-4dd0-8d2d-bf7647997877")
+		assert.Equal(t, ev.Address[0], "127.0.0.1")
+	})
+
+	t.Run("Receive event over Handler", func(t *testing.T) {
+		go func() {
+			defer wg.Done()
+			x := <-ch
+			assert.Equal(t, "Scheduled", x.Reason)
+		}()
+
+		wg.Add(1)
+		h := &Handler{&kubernetesClient{}, ch, mCache}
+		h.OnAdd(e.Items[0])
+		wg.Wait()
+	})
 }
