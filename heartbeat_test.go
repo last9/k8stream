39813071cfe9424832e502a92809dac67f0d99c7
talina06 <talina.shrotriya@gmail.com>
2020-04-17 16:15:33 +0530

    On upgrade, invoke non-zero exit and shutdown of k8stream.

--- heartbeat_test.go
+++ heartbeat_test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"os/signal"
+	"syscall"
 	"testing"
 	"time"
 
@@ -12,8 +15,13 @@ import (
 
 func TestStartHeartbeat(t *testing.T) {
 	uids := make(chan string, 2)
+	upgradeUid := "test"
 	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		uid := r.URL.Query().Get("uid")
+		if uid == upgradeUid {
+			w.WriteHeader(http.StatusUpgradeRequired)
+			return
+		}
 		if uid != "" {
 			uids <- uid
 		}
@@ -24,7 +32,7 @@ func TestStartHeartbeat(t *testing.T) {
 	uid := string(uuid.NewUUID())
 	interval := 1
 
-	assert.Nil(t, StartHeartbeat(uid, s.URL, interval))
+	assert.Nil(t, StartHeartbeat(uid, s.URL, interval, 0))
 
 	select {
 	case received := <-uids:
@@ -33,4 +41,23 @@ func TestStartHeartbeat(t *testing.T) {
 	case <-time.After(time.Duration(interval+3) * time.Second):
 		t.Error("no heartbeat in expected interval")
 	}
+
+	t.Run("upgrade should send SIGQUIT to main process", func(t *testing.T) {
+		sigCh := make(chan os.Signal)
+		signal.Notify(sigCh, syscall.SIGQUIT)
+
+		if err := StartHeartbeat(upgradeUid, s.URL, interval, 0); err != nil {
+			t.Fatal(err)
+		}
+
+		select {
+		case <-time.After(3 * time.Second):
+			t.Fatal("should have received a SIGQUIT")
+		case <-sigCh:
+			return
+		}
+
+		t.Fatal("should have received a SIGQUIT")
+	})
+
 }
