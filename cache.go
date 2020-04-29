e155f8590c3d4c1d242b410546bc1274f7ba6e5f
Piyush Verma <github@piyushverma.net>
2020-04-29 12:20:45 +0530

    Add a new cache / db interface

--- cache.go
+++ cache.go
@@ -1,11 +1,102 @@
 package main
 
-import "github.com/dgraph-io/ristretto"
+import (
+	"encoding/json"
+	fmt "fmt"
+	"time"
 
-func cacheClient() (*ristretto.Cache, error) {
-	return ristretto.NewCache(&ristretto.Config{
-		NumCounters: 1e7, // number of keys to track frequency of (10M).
-		MaxCost:     10000000,
-		BufferItems: 64, // number of keys per Get buffer.
+	"github.com/tidwall/buntdb"
+)
+
+// Multiple read-only transactions can be opened at the same time but
+// there can only be one read/write transaction at a time.
+// Attempting to open a read/write transactions while another one is
+// in progress will result in blocking until the current read/write
+// transaction is completed.
+// Use sync.Mutex underneath. Will come up with something else later.
+type Cache struct {
+	db *buntdb.DB
+}
+
+// Item that is internally saved to the database.
+// Don't expect the Uid to be generated on Insert.
+type dbItem struct {
+	Uid string      `json:"uid"`
+	Obj interface{} `json:"obj"`
+}
+
+func makeKey(table, uid string) string {
+	return fmt.Sprintf("%s-%s", table, uid)
+}
+
+func (c *Cache) Get(table, uid string, obj interface{}) (bool, error) {
+	key := makeKey(table, uid)
+	var missing bool
+
+	return missing, c.db.View(func(tx *buntdb.Tx) error {
+		val, err := tx.Get(key) //IgnoreExpiredValue
+		if err != nil {
+			// NOTE: Weird syntax by buntDB where it returns a pre-constructed
+			// value rather than an error type.
+			if err == buntdb.ErrNotFound {
+				missing = true
+			}
+			return err
+		}
+
+		return json.Unmarshal([]byte(val), obj)
 	})
+}
+
+// Set is just a wrapper around ExpireSet with a no-expiration Intent.
+func (c *Cache) Set(table, uid string, obj interface{}) error {
+	return c.ExpireSet(table, uid, obj, 0)
+}
+
+func (c *Cache) ExpireSet(table, uid string, obj interface{}, expires int) error {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+
+	opts := &buntdb.SetOptions{}
+	if expires > 0 {
+		opts.Expires = true
+		opts.TTL = time.Duration(expires) * time.Second
+	}
+
+	return c.db.Update(func(tx *buntdb.Tx) error {
+		indices, err := tx.Indexes()
+		if err != nil {
+			return err
+		}
+
+		var indexExists bool
+		for _, ix := range indices {
+			if table == ix {
+				indexExists = true
+				break
+			}
+		}
+
+		if !indexExists {
+			if err := tx.CreateIndex(table, makeKey(table, "*"), buntdb.IndexString); err != nil {
+				return err
+			}
+		}
+
+		tx.Set(makeKey(table, uid), string(b), opts)
+		return nil
+	})
+}
+
+type Cachier interface {
+	Set(table, uid string, obj interface{}) error
+	ExpireSet(table, uid string, obj interface{}, expires int) error
+	Get(table, uid string, obj interface{}) (bool, error)
+}
+
+func newCache() (Cachier, error) {
+	db, err := buntdb.Open(":memory:")
+	return &Cache{db}, err
 }
---
+++ cache_test.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"log"
+	"strconv"
+	"sync"
+	"testing"
+
+	"gopkg.in/go-playground/assert.v1"
+)
+
+type testItem struct {
+	Foo string
+	Id  int
+}
+
+func TestCache(t *testing.T) {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	c, err := newCache()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Internal tests", func(t *testing.T) {
+		t.Run("Set a key", func(t *testing.T) {
+			var wg sync.WaitGroup
+			for ix := 0; ix < 4; ix++ {
+				wg.Add(1)
+				go func(val int) {
+					defer wg.Done()
+					if err := c.Set("table", strconv.Itoa(val), val); err != nil {
+						t.Fatal(err)
+
+					}
+				}(ix)
+			}
+
+			wg.Wait()
+
+			// Should run the next few gets in Parallel
+			t.Parallel()
+
+			for ix := 0; ix < 4; ix++ {
+				t.Run("Get a key", func(t *testing.T) {
+					var val int
+					missing, err := c.Get("table", strconv.Itoa(ix), &val)
+					if missing {
+						t.Fatal("Value", ix, "is missing")
+					} else if err != nil {
+						t.Fatal(err)
+					} else {
+						assert.Equal(t, ix, val)
+					}
+				})
+			}
+		})
+	})
+
+	t.Run("Insert a random struct", func(t *testing.T) {
+		if err := c.Set("test", "uid1", &testItem{Foo: "foo", Id: 1}); err != nil {
+			t.Fatal(err)
+		}
+
+		t.Run("Fetch the struct", func(t *testing.T) {
+			var ret testItem
+			if _, err := c.Get("test", "uid1", &ret); err != nil {
+				t.Fatal(err)
+			}
+		})
+	})
+}
